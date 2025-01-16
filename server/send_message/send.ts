022918ad0958d5487a744359297d871b682d14e5
Yashasvi <yashasvibansal03@gmail.com>
2025-01-16 14:38:22 +0530

    Test: Add all the cases for handling differnet type of message

--- server/send_message/send.ts
+++ server/send_message/send.ts
@@ -27,36 +27,84 @@
 //     return response;
 // };
 
-// export const replyToComments = async (commentId: string, message: string): Promise<Response> => {
-//     const response = await fetch(`${baseUrl}/send_message/send`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//             commentId,
-//             message,
-//         }),
-//     });
-//     return response;
-// };
+const handleComment =  (commentData: any):object  => {
+    const reelId = commentData.value.media.id;
+    const recipientId = commentData.value.from.id;
+    const message = commentData.value.text;
+    let reply = `You commented: ${message} on this reel: ${reelId}`;
+    return {
+        "recipient": {
+            "id": recipientId
+        },
+        "message": {
+            "text": reply
+        }
+    };
+};
+
+const handleReelShared =  (messageData: any): object => {
+    const reelId = messageData.message.attachments[0].payload.reel_video_id;
+    const recipientId = messageData.sender.id;
+    const reply = `You shared this reel: ${reelId}`;
+    return {
+        "recipient": {
+            "id": recipientId
+        },
+        "message": {
+            "text": reply
+        }
+    };
+};
+
+const handelMessage = (messageData: any): object => {
+    const recipientId = messageData.sender.id;
+    const message = messageData.message.text;
+    const reply = `You sent: ${message}`;
+    return {
+        "recipient": {
+            "id": recipientId
+        },
+        "message": {
+            "text": reply
+        }
+    };
+};
+
+
+
+const checkWebhookType = (data: any) => {
+    let payload = null
+    if (data.entry[0].changes?.[0]) {
+        payload = handleComment(data.entry[0].changes[0]);
+    } else {
+        if (data.entry[0].messaging[0].message?.attachments?.[0]?.type === 'ig_reel') {
+            payload = handleReelShared(data.entry[0].messaging[0]);
+        } else {
+            if (data.entry[0].messaging[0].message?.text && data.entry[0].messaging[0].recipient?.id === process.env.INSTAGRAM_ACCOUNT_ID) {
+                payload = handelMessage(data.entry[0].messaging[0]);
+            }
+        }
+    }
+    return payload;
+}
 
 const sendMessage = async (data: any) => {
     try {
+        // const info = data.entry[0].messaging[0];
         const baseUrl = "https://graph.instagram.com/v21.0";
         const IGId = process.env.INSTAGRAM_ACCOUNT_ID;
-        const info = data.entry[0].messaging[0];
         const token = process.env.INSTAGRAM_ACCESS_TOKEN;
-        const recipientId = info.sender.id;
+        // const recipientId = info.sender.id;
         const message = "this is A test message";
-        const payload = {
-            "recipient": {
-                "id": recipientId
-            },
-            "message": {
-                "text": message
-            }
-        };
+        // const payload = {
+        //     "recipient": {
+        //         "id": recipientId
+        //     },
+        //     "message": {
+        //         "text": message
+        //     }
+        // };
+        const payload = checkWebhookType(data);
 
         const url = `${baseUrl}/${IGId}/messages`;
         console.log('url is ', url);
